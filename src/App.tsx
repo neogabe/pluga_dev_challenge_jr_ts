c321edaef6f797ff268c02fce44b69f9b8c458b8
Gabriel <glima7591@gmail.com>
2025-04-23 13:43:13 -0300

    feat: adicionar layout principal com título e descrição
    - Modifica componente App com layout básico estilizado com Tailwindcss
    - Adicionado título 'Ferramentas Pluga' e descrição introdutória

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-center py-8 text-blue-600">
+        Ferramentas Pluga
+      </h1>
+      <p className="text-center text-gray-600 mb-8">
+      Integre em um passe de mágica as ferramentas web que você mais usa. 💙
+      </p>
+    </main>
   )
 }
 
